e5857081db1f6dcd92cccc882841e0638755cb77
Kosta Harlan <kostajh@gmail.com>
2013-01-19 18:59:42 -0500

    Add annotation support

--- sublime-taskwarrior.py
+++ sublime-taskwarrior.py
@@ -132,7 +132,7 @@ class TaskwarriorViewTasksCommand (sublime_plugin.WindowCommand):
 
         twtask = twtasks[idx - 3]
         self.tasktitle = twtask[u'description']
-        self.mod_options = [self.tasktitle, u'\u21b5' + ' Back to Tasks', u'\u2714' + ' Done', u'\u270E' + ' Modify', u'\u2715' + ' Delete']
+        self.mod_options = [self.tasktitle, u'\u21b5' + ' Back to Tasks', u'\u2714' + ' Done', u'\u270E' + ' Modify', u'\u270E' + ' Annotate', u'\u2715' + ' Delete']
 
         self.window.show_quick_panel(self.mod_options, self.mod_task, sublime.MONOSPACE_FONT)
 
@@ -150,7 +150,7 @@ class TaskwarriorViewTasksCommand (sublime_plugin.WindowCommand):
 
         # Mark Task as done
         if idx == 2:
-            # @todo use TaskWarrior API?
+            # @todo use Taskw
             subprocess.call(['task', twtask[u'uuid'], 'done'])
             self.get_tasks(self.quick_panel_project_selected_index)
 
@@ -158,9 +158,14 @@ class TaskwarriorViewTasksCommand (sublime_plugin.WindowCommand):
         if idx == 3:
             self.window.run_command('taskwarrior_modify_task_from_input')
 
-        # Delete Task
+        # Annotate Task
         if idx == 4:
-            # @todo use Taskwarrior API?
+            self.window.run_command('taskwarrior_annotate_task_from_input')
+
+        # Delete Task
+        if idx == 5:
+            # @todo use Taskw
+            # @todo add confirmation
             subprocess.call('yes | task ' + twtask[u'uuid'] + ' delete', shell=True)
             self.get_tasks(self.quick_panel_project_selected_index)
 
@@ -177,8 +182,22 @@ class TaskwarriorAddTaskFromInputCommand(sublime_plugin.WindowCommand):
     def on_done(self, input):
         if input != '':
             self.task['title'] = input
-            #todo use TaskWarrior API? This is more flexible though.
             subprocess.call(['task', 'add', input])
+            self.window.run_command('taskwarrior_view_tasks', {'resetTasks': True})
+        pass
+
+
+class TaskwarriorAnnotateTaskFromInputCommand(sublime_plugin.WindowCommand):
+
+    global twtask
+
+    def run(self):
+        self.window.show_input_panel('Annotate "' + twtask[u'description'] + '"', "", self.on_done, None, None)
+        pass
+
+    def on_done(self, input):
+        if input != '':
+            subprocess.call(['task', twtask[u'uuid'], 'annotate', input])
             self.window.run_command('taskwarrior_view_tasks', {'resetTasks': True})
         pass
 
@@ -194,7 +213,6 @@ class TaskwarriorModifyTaskFromInputCommand(sublime_plugin.WindowCommand):
     def on_done(self, input):
         if input != '':
             twtask[u'description'] = input
-            #todo use TaskWarrior API?
             subprocess.call(['task', twtask[u'uuid'], 'mod', input])
             self.window.run_command('taskwarrior_view_tasks', {'resetTasks': True})
         pass
